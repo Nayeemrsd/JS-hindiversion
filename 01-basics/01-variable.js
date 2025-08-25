2f3fbc9e3a3f223ce13123719c8945dc2f26cb52
Nayeemrsd <129419064+Nayeemrsd@users.noreply.github.com>
2025-08-25 14:46:40 +0000

    changes

---
+++ 01-basics/01-variable.js
@@ -0,0 +1,17 @@
+const accountId = 123456
+let accountEmail ="nayeemmirza633@gmail.com"
+var accountPassword ="987916"
+accountCity = "Nizamabad"
+let accountState
+
+/* accountId = 234555 not allowed because const cannot be change */
+
+accountEmail ="mirzanayeem633@gmail.com"
+accountPassword ="9879164117"
+accountCity ="Hyderabad"
+
+// We cannot use var keyword because it has issue of block scope and functional scope //
+// whenever we didnot assign any value to a variable it shows undefined //
+
+ 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
