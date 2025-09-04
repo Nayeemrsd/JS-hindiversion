04252ed58b9c731f5cf6c565a67c61b61bac4c72
Nayeemrsd <129419064+Nayeemrsd@users.noreply.github.com>
2025-09-04 02:42:33 +0000

    while loop

---
+++ 05-iteration/02-two.js
@@ -0,0 +1,32 @@
+// while do while loop
+// let index = 0
+// while (index <= 10) {
+//     console.log(`the index value is ${index}`);
+    
+//     index = index +2
+// }
+
+// let myArray = ["thor", "hulk",'ironman', 'flash']
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`super hero is ${myArray[arr]}`);
+//     arr = arr +1
+    
+// }
+// let myNum = 0
+// do {
+//    console.log(`my number is ${myNum}`);
+//    myNum++
+    
+// } while (myNum <= 10);
+// if the initial value of myNum is 11 the do while loop will print 11 
+// because name itself says that do then while loop it will do first
+
+let myNum = 11
+do {
+   console.log(`my number is ${myNum}`);
+   myNum++
+    
+} while (myNum <= 10);
+
+// output is "my number is 11" remember
